2574e192a61e3bd6627393742e688486131319dc
bakerjaid <jaidencolebaker@gmail.com>
2018-07-17 15:48:45 +1200

    Added integrated databases

--- src/jdz/UEconomy/data/UEcoDatabase.java
+++ src/jdz/UEconomy/data/UEcoDatabase.java
@@ -4,15 +4,25 @@ package jdz.UEconomy.data;
 import java.util.List;
 import java.util.UUID;
 
+import jdz.UEconomy.UEco;
+import jdz.bukkitUtils.misc.Config;
+
 interface UEcoDatabase {
+	public static final boolean useSQL = Config.getSQLConfig(UEco.getInstance()).isPreferSQL();
+
 	public static UEcoDatabase getInstance() {
-		return null; // TODO
+		if (useSQL)
+			return UEcoDatabaseSQL.getInstance();
+		return UEcoDatabaseYML.getInstance();
 	}
-	
+
 	public UEcoEntry get(UUID uuid);
+
 	public UEcoEntry get(String name);
+
 	public List<UEcoEntry> getAll();
-	
+
 	public void set(UUID uuid, double amount);
+
 	public void add(UUID uuid, double amount);
 }
