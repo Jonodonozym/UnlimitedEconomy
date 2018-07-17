71b1483e24e7977deb753978d30f19fc35efe58c
bakerjaid <jaidencolebaker@gmail.com>
2018-07-17 14:56:46 +1200

    Linked everything together

---
+++ src/jdz/UEconomy/UEco.java
@@ -0,0 +1,42 @@
+package jdz.UEconomy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import jdz.UEconomy.commands.BalanceCommand;
+import jdz.UEconomy.commands.BaltopCommand;
+import jdz.UEconomy.commands.PayCommand;
+import jdz.UEconomy.commands.UEcoCommandExecutor;
+import jdz.UEconomy.data.UEcoBank;
+import jdz.UEconomy.data.UEcoTop;
+import jdz.UEconomy.vault.UEcoVaultHook;
+import lombok.Getter;
+import net.milkbowl.vault.economy.Economy;
+
+public class UEco extends JavaPlugin {
+	@Getter private static UEco instance;
+
+	@Override
+	public void onEnable() {
+		instance = this;
+
+		UEcoCommandExecutor.getInstance().register();
+		new BalanceCommand().register(this);
+		new BaltopCommand().register(this);
+		new PayCommand().register(this);
+		
+		new UEcoBank().registerEvents(this);
+		
+		UEcoTop.update();
+
+		if (getServer().getPluginManager().getPlugin("Vault") != null)
+			Bukkit.getServer().getServicesManager().register(Economy.class, UEcoVaultHook.getInstance(), this,
+					ServicePriority.High);
+		
+		for (Player player: Bukkit.getOnlinePlayers())
+			new UEcoBank().onJoin(new PlayerJoinEvent(player, ""));
+	}
+}
